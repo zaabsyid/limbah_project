535bc47d18d9749adf683d2a57c7d5398064dadf
zaabsyid <abdulrosyid2002@gmail.com>
2024-10-21 20:52:29 +0700

    add dashboard niceadmin template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'indexPage']);
