af9566ffd68db6effc7785f14706ad892ea78d86
reonaldi152 <reonaldi@team.superfk.co>
2024-11-05 20:56:18 +0700

    fixing migration perpanjangan mou

--- database/migrations/2024_11_05_130646_create_perpanjangan_mous_table.php
+++ database/migrations/2024_11_05_130646_create_perpanjangan_mous_table.php
@@ -16,10 +16,6 @@ return new class extends Migration
             $table->foreignId('mou_id')->constrained('mous')->onDelete('cascade');
             $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
             $table->enum('package', ['2', '5']);
-            $table->integer('year')->nullable(); // Tahun perpanjangan (1 hingga 5)
-            $table->enum('status', ['orange', 'green'])->default('orange'); // Status pembayaran
-            $table->date('due_date'); // Tanggal jatuh tempo untuk setiap tahun
-            $table->string('document_payment')->nullable(); // Bukti pembayaran
             $table->boolean('notified')->default(false);
             $table->timestamps();
         });
