798b98789197df1f8407eb5fd7e66de561cb4094
reonaldi152 <reonaldi@team.superfk.co>
2024-11-05 19:42:53 +0700

    fixing migration

--- database/migrations/2024_10_27_035421_create_customers_table.php
+++ database/migrations/2024_10_27_035421_create_customers_table.php
@@ -18,10 +18,12 @@ return new class extends Migration
             $table->string('phone');
             $table->text('address');
             $table->string('occupation');
+            $table->string('ktp_image');
             $table->string('nik');
             $table->string('str_sip');
+            $table->string('str_sip_image');
+            $table->string('npwp_image');
             $table->string('npwp');
-            $table->string('image');
             $table->foreignId('province_id')->constrained('provinces')->onDelete('cascade');
             $table->foreignId('city_id')->constrained('cities')->onDelete('cascade');
             $table->softDeletes();
--- database/migrations/2024_10_30_140440_create_billings_table.php
+++ database/migrations/2024_10_30_140440_create_billings_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('billings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('customer_id')->constrained('customers')->onDelete('cascade');
-            $table->date('due_date');
+            $table->string('document_payment');
             $table->enum('status', ['putus_kontrak', 'sudah_perpanjang', 'belum_diperpanjang'])->default('belum_diperpanjang');
             $table->timestamps();
             $table->softDeletes();
